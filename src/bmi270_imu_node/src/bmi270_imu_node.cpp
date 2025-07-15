8e1c567a5ea85459b6e3aa564f4a22e663701081
diplodocuslongus <hinoki.kuaimu@gmail.com>
2025-07-15 16:02:38 +0800

    ODR

--- src/bmi270_imu_node/src/bmi270_imu_node.cpp
+++ src/bmi270_imu_node/src/bmi270_imu_node.cpp
@@ -79,7 +79,7 @@ public:
 
         // Accelerometer configuration
         config[0].type = BMI2_ACCEL;
-        config[0].cfg.acc.odr = BMI2_ACC_ODR_200HZ; // Output Data Rate: 100Hz
+        config[0].cfg.acc.odr = BMI2_ACC_ODR_800HZ; // Output Data Rate defined in bmi2_defs.h
         config[0].cfg.acc.range = BMI2_ACC_RANGE_4G; // Range: +/- 4G
         config[0].cfg.acc.bwp = BMI2_ACC_NORMAL_AVG4; // Bandwidth parameter: Normal average 4
         config[0].cfg.acc.filter_perf = BMI2_PERF_OPT_MODE; // Performance mode: Optimized
@@ -87,7 +87,7 @@ public:
         // Gyroscope configuration
         // all hardcoded for now, TODO see how to use a parameter file to set them 
         config[1].type = BMI2_GYRO;
-        config[1].cfg.gyr.odr = BMI2_GYR_ODR_200HZ; // Output Data Rate: 100Hz
+        config[1].cfg.gyr.odr = BMI2_GYR_ODR_800HZ; // Output Data Rate defined in bmi2_defs.h
         config[1].cfg.gyr.range = BMI2_GYR_RANGE_2000; // Range: +/- 2000 degrees per second (dps)
         config[1].cfg.gyr.bwp = BMI2_GYR_NORMAL_MODE; // Bandwidth parameter: Normal mode
         config[1].cfg.gyr.noise_perf = BMI2_PERF_OPT_MODE; // Noise performance mode: Optimized
